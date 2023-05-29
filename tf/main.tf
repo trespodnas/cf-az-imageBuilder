0ebd3ba40456f5159aca5c017c7f6b560f0de8b8
Jay <jnelson@LTCF99.local>
2023-05-28 22:55:13 -0400

    updated to add creation of images in gallery

---
+++ tf/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_shared_image" "main" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  gallery_name        = var.gallery_name
+  location            = var.location
+  os_type             = var.os_type
+  identifier {
+    offer     = var.image_offer
+    publisher = var.image_publisher
+    sku       = var.image_sku
+  }
+}
---
+++ tf/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  description = "shared image name"
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "resource group name"
+  type = string
+}
+
+variable "gallery_name" {
+  description = "image gallery name"
+  type = string
+}
+
+variable "location" {
+  description = "resource group location"
+  type = string
+}
+
+variable "os_type" {
+  description = "OS type"
+  type = string
+}
+
+variable "image_offer" {
+  description = "image offer"
+  type = string
+}
+
+variable "image_publisher" {
+  description = "image publisher"
+  type = string
+}
+
+variable "image_sku" {
+  description = "image sku"
+  type = string
+}
